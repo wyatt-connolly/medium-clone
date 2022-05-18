715417bde1653c98f00a6cd20b027c4e06707473
MassyWeeWee <wyattconnollyfitness@gmail.com>
2022-05-18 11:42:25 -0700

    added slug page and homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,11 +39,31 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6 ">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
-              <img src={urlFor(post.mainImage).url()!} alt="" />
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p
+                    className="text-xs
+                  "
+                  >
+                    {post.description} by {post.title}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()}
+                  alt=""
+                />
+              </div>
             </div>
           </Link>
         ))}
